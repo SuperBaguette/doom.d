27e536f22a2f109ce6a441d4ecb05c8329c6926b
Nicolas FABBRO <nicolas.fabbro@gmail.com>
2022-09-09 15:00:20 +0200

    add packages to make org-mode prettier

--- packages.el
+++ packages.el
@@ -9,6 +9,14 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
+;; Org-mode
+(package! org-beautify-theme)
+(package! org-bullets)
+(package! org-link-beautify)
+(package! org-tag-beautify)
+(package! org-ref-prettify)
+(package! org-superstar)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
